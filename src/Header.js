21f375b9ec2400d8256ffd4598c51f6edbe41305
creator54 <hi.creator54@gmail.com>
2021-08-12 14:50:41 +0530

    learned a bit about hooks ; finally clicking on anchor tag works ;)

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React,{useState} from 'react'
 import Main from './Main'
+import DataTable from './DataTable'
 
-class Header extends Component{
-  handleClick(){
-    return (<Main/>)
-  }
+function Header() {
+  const [state, setstate] = useState('main')
 
-  render(){
-    return(
-      <div className='navbar'>
-        <img class='logo' src='https://static.vecteezy.com/system/resources/previews/000/620/462/large_2x/sun-generic-logo-and-symbols-vector.jpg' alt='logo'/>
-        <header>
-          <a>SF BANK</a>
-          <a href='' id='navbar-right' onClick={this.handleClick}>Customers</a>
-        </header>
-      </div>
-    )
-  }
+  return(
+    <div className='navbar'>
+      <img className='logo' src='https://static.vecteezy.com/system/resources/previews/000/620/462/large_2x/sun-generic-logo-and-symbols-vector.jpg' alt='logo'/>
+      <header>
+        <a href='#' onClick={() => setstate('main')}>SF BANK</a>
+        <a href='#' id='navbar-right' onClick={() => setstate('DataTable')}>Customers</a>
+      </header>
+      {state==='main'?<Main/>:<DataTable/>}
+    </div>
+  )
 }
 
 export default Header
