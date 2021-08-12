7864eade696aaf4cdcbcc6a8cea6eb376aaf794b
creator54 <hi.creator54@gmail.com>
2021-08-12 14:56:39 +0530

    some cleanup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './Header'
-import Main from './Main'
-import DataTable from './DataTable'
 import Footer from './Footer'
 ReactDOM.render(
   <div>
     <Header/>
-    {/* <Main/> */}
-    {/* <DataTable/> */}
     <Footer/>
   </div>,
   document.getElementById("root")
