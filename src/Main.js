03f4704d0c0889619cf1d42dcb63b058f98bd930
creator54 <hi.creator54@gmail.com>
2021-08-13 01:08:11 +0530

    UI updates

--- src/Main.js
+++ src/Main.js
@@ -3,31 +3,38 @@ import React from 'react'
 function Main(){
   const date = new Date()
   let time
-  // let source
+  let source
 
   if (date.getHours() < 12) {
     time = 'Morning'
-    // source = 'https://image.flaticon.com/icons/png/128/3892/3892928.png'
+    source = 'https://image.flaticon.com/icons/png/128/3892/3892928.png'
   }
   else if (date.getHours() >= 12 && date.getHours() < 17) {
     time = 'Afternoon'
-    // source = 'https://image.flaticon.com/icons/png/128/2698/2698194.png'
+    source = 'https://image.flaticon.com/icons/png/128/2698/2698194.png'
   }
   else if (date.getHours() >= 17 && date.getHours() < 20){
     time = 'Evening'
-    // source = 'https://image.flaticon.com/icons/png/128/3094/3094159.png'
+    source = 'https://image.flaticon.com/icons/png/128/3094/3094159.png'
   }
   else {
     time = 'Night'
-    // source = 'https://image.flaticon.com/icons/png/128/3626/3626276.png'
+    source = 'https://image.flaticon.com/icons/png/128/3626/3626276.png'
   }
 
-  const user = 'Creator54'
+  const styles = {
+    top:'5px',
+    right:'-90px',
+    position:'absolute',
+    fontSize:'15px',
+    height:'80px',
+    width: '80px'
+  }
 
   return(
     <main>
-      {/* <img style={styles} src={source} alt={time}/> */}
-      Good {time} {user}
+      We hope you are having a wonderful {time} 
+      <img style={styles} src={source} alt={time}/>
       <div id='image-container'>
         <img id='image' src='https://logodix.com/logo/871229.jpg' alt='bank'/>
       </div>
